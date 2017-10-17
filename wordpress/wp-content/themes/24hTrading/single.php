322e0c5f6892382be45cf4dc4406e62e5134bdd2
Alessandro Pistola <pistola.alessandro@virgilio.it>
2017-10-17 18:24:26 +0200

    added commets function

---
+++ wordpress/wp-content/themes/24hTrading/single.php
@@ -0,0 +1,46 @@
+<?php get_header(); ?>
+
+
+      <!-- START THE FEATURETTES -->
+      <hr class="featurette-divider">
+      <?php if(have_posts()) : ?>
+
+        <?php while(have_posts()) : the_post(); ?>
+
+      <div class="row featurette">
+        <div class="col-md-8">
+          <h2 class="featurette-heading">
+             <?php the_title(); ?>
+          </h2>
+          <span class="meta-data-post">
+            <?php the_time('F j, Y g:i a'); ?>
+            by <a href="<?php echo get_author_posts_url(get_the_author_meta('ID')); ?>">
+              <?php the_author(); ?>
+            </a>
+          </span>
+          <p class="lead"> <?php the_content(); ?> </p>
+        </div>
+
+        <div class="col-md-4">
+          <?php if(has_post_thumbnail()) : ?>
+            <div class="thumb">
+              <?php the_post_thumbnail(); ?>
+            </div>
+            <?php else: ?>
+            <img class="featurette-image img-fluid mx-auto" data-src="holder.js/500x450/auto" alt="Banner">
+          <?php endif; ?>
+        </div>
+      </div>
+
+      <hr class="featurette-divider">
+
+        <?php comments_template(); ?>
+
+      <hr class="featurette-divider">
+      <?php endwhile; ?>
+    <?php else :  ?>
+      <p> <?php __('No posts Found');?> </p>
+    <?php endif; ?>
+      <!-- /END THE FEATURETTES -->
+
+<?php get_footer();?>
